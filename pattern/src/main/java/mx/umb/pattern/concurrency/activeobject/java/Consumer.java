49e6e40ee5d29dde7ee84ed0221d68e180a62fc6
MXE01503927A <MXE01503927A@MXE01503927A.ASEFI.MEX>
2015-09-09 15:20:03 -0500

    commit active object pattern

---
+++ pattern/src/main/java/mx/umb/pattern/concurrency/activeobject/java/Consumer.java
@@ -0,0 +1,23 @@
+package mx.umb.pattern.concurrency.activeobject.java;
+
+import java.util.concurrent.BlockingQueue;
+
+public class Consumer implements Runnable {
+	protected BlockingQueue queue = null;
+
+	public Consumer(BlockingQueue queue) {
+		this.queue = queue;
+	}
+
+	@Override
+	public void run() {
+		try {
+			System.out.println(queue.take());
+			System.out.println(queue.take());
+			System.out.println(queue.take());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ pattern/src/main/java/mx/umb/pattern/concurrency/activeobject/java/Producer.java
@@ -0,0 +1,25 @@
+package mx.umb.pattern.concurrency.activeobject.java;
+
+import java.util.concurrent.BlockingQueue;
+
+public class Producer implements Runnable {
+	protected BlockingQueue queue = null;
+
+	public Producer(BlockingQueue queue) {
+		this.queue = queue;
+	}
+
+	@Override
+	public void run() {
+		try {
+			queue.put("1");
+			Thread.sleep(1000);
+			queue.put("2");
+			Thread.sleep(1000);
+			queue.put("3");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
