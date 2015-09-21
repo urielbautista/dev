7542a207beae714ab52dfc9008c7ce820420a981
MXE01503927A <MXE01503927A@MXE01503927A.ASEFI.MEX>
2015-09-21 12:08:24 -0500

    commit event model pattern

--- pattern/src/main/java/mx/umb/pattern/concurrency/eba/eventmodel/NumberReadListener.java
+++
@@ -1,9 +0,0 @@
-package mx.umb.pattern.concurrency.eba.eventmodel;
-
-import java.util.EventListener;
-
-public interface NumberReadListener extends EventListener {
-    public void numberRead(NumberReadEvent numberReadEvent);
-   
-    public void numberStreamTerminated(NumberReadEvent numberReadEvent);
-}
