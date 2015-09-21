aa2636f17a5f3504a98f811601de8fc734879880
MXE01503927A <MXE01503927A@MXE01503927A.ASEFI.MEX>
2015-09-21 11:42:21 -0500

    commit event model pattern

---
+++ pattern/src/main/java/mx/umb/pattern/concurrency/eba/eventmodel/NumberReadListenerImpl.java
@@ -0,0 +1,16 @@
+package mx.umb.pattern.concurrency.eba.eventmodel;
+
+public class NumberReadListenerImpl  implements NumberReadListener{
+	Double totalSoFar = 0D;
+
+	@Override
+	public void numberRead(NumberReadEvent numberReadEvent) {
+		totalSoFar += numberReadEvent.getNumber();
+	}
+
+	@Override
+	public void numberStreamTerminated(NumberReadEvent numberReadEvent) {
+		System.out.println("Sum of the number stream: " + totalSoFar);
+	}
+	
+}
